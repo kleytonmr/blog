46e29c58e3ead2dfe405765afa13ceee78c50483
Rafael Possatto <rafaelpossatto@gmail.com>
2018-02-27 21:19:04 -0300

    Corrige sintaxe e melhora a legibilidade

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,22 @@
 class User < ApplicationRecord
     has_secure_password
-    validates name, presence: true, length: {maximum: 50}
-    validates password, presence: false, length: {minimum: 6}
-    VALID_EMAIL_FORMAT= /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
-    validates email, presence: true, length: {maximum: 260}, format: { with: VALID_EMAIL_FORMAT}, uniqueness: {case_sensitive: false}
-    before_save { self.email = email.downcase }
+
+    VALID_EMAIL_FORMAT = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+
+    with_options presence: true do
+      validates :name, length: { maximum: 50 }
+      validates :password, length: { minimum: 6 }
+      validates :email,
+        length: { maximum: 260 },
+        format: { with: VALID_EMAIL_FORMAT},
+        uniqueness: { case_sensitive: false }
+    end
+
+    before_save { email_downcase }
+
+    private
+
+    def email_downcase
+      self.email.downcase!
+    end
 end
