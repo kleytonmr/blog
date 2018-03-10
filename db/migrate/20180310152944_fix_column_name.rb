9bee5172298c849ff53d87c91d1858de383e9e15
Kleyton ramos <kleytonmr@ucl.br>
2018-03-10 14:00:05 -0300

    Correções no genero e form

--- db/migrate/20180310152944_fix_column_name.rb
+++ db/migrate/20180310152944_fix_column_name.rb
@@ -1,5 +1,5 @@
 class FixColumnName < ActiveRecord::Migration[5.1]
   def change
-  	rename_column :users, :sex, :gender
+  	rename_column :users, :sex, :gender 
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180309034003) do
+ActiveRecord::Schema.define(version: 20180310152944) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20180309034003) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
-    t.string "sex"
+    t.string "gender"
   end
 
 end
