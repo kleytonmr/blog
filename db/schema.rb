9f3138a4baa169d14b54c5aaa95ba3f9ab2bde72
Kleyton ramos <kleytonmr@ucl.br>
2018-03-11 18:56:22 -0300

    Configura projeto

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180310152944) do
+ActiveRecord::Schema.define(version: 20180309034003) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,7 +21,7 @@ ActiveRecord::Schema.define(version: 20180310152944) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
-    t.string "gender"
+    t.boolean "gender"
   end
 
 end
