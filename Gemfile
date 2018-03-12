18ebfad9150234921edc3e898c84c2bda12924bf
Kleyton ramos <kleytonmr@ucl.br>
2018-03-12 02:57:58 -0300

    Add navbar

--- Gemfile
+++ Gemfile
@@ -32,6 +32,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'bcrypt', '~> 3.1.7'
 # bootstrap
 gem 'bootstrap', '~> 4.0.0'
+# Bootstrap JavaScript depends
+gem 'jquery-rails'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'users#index'
 
   get    'index'  => 'users#index' 
-  get	   'signup' => 'users#new'
+  get	 'signup' => 'users#new'
   get    'login'  => 'sessions#new'
   post   'login'  => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
